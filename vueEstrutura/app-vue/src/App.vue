caa1acdcd97a26afcac0641046dd5a9a7cc4de32
Michel Amorim <michelmsa7799@gmail.com>
2022-09-28 10:31:36 -0300

    vue estrutura

---
+++ vueEstrutura/app-vue/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div id="app"></div>
+    <HeaderComponent />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
---
+++ vueEstrutura/app-vue/src/components/HeaderComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <ul class="container">
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>Serviços</a>
+      </li>
+      <li>
+        <a>Contatos</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style>
+ul {
+  display: flex;
+  list-style: none;
+}
+li {
+  padding: 10px;
+}
+</style>
