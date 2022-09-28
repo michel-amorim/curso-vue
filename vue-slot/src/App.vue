56201ac7e9e4565d0a48016e7b64803bee4da403
Michel Amorim <michelmsa7799@gmail.com>
2022-09-28 10:54:27 -0300

    slots

---
+++ vue-slot/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <ModalPrincipal>
+      <template v-slot:header="{ logo }">
+        <p>
+          Esse é o header: <strong>{{ logo }}</strong>
+        </p>
+      </template>
+      <template v-slot:default="{ user }">
+        {{ user.nome }}
+        <p>teste</p>
+        <button>Comprar</button>
+      </template>
+      <template #footer> <p>Esse é o footer</p> </template>
+    </ModalPrincipal>
+  </div>
+</template>
+
+<script>
+import ModalPrincipal from "./components/ModalPrincipal.vue";
+export default {
+  name: "App",
+  components: {
+    ModalPrincipal,
+  },
+};
+</script>
+
+<style scoped>
+strong {
+  font-size: 2rem;
+}
+</style>
---
+++ vue-slot/src/components/ModalPrincipal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="modal">
+    <slot name="header" :logo="logo"></slot>
+    <h1>Modal Principal</h1>
+    <slot :user="user"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalPrincipal",
+  data() {
+    return {
+      user: {
+        nome: "Michel",
+      },
+      logo: "ADIDAS",
+    };
+  },
+};
+</script>
+
+<style></style>
